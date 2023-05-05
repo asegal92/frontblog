3f727b42c17f5088a0c22ea6d1d7fdab7c61afd6
anastasiia92 <nastia.dmytryshyna@gmail.com>
2023-05-05 00:27:18 -0400

    single post done

--- blogfront/src/pages/SinglePost.js
+++ blogfront/src/pages/SinglePost.js
@@ -1,7 +1,22 @@
-import React from "react";
+import {useMemo} from 'react'
+import { Link, useParams } from "react-router-dom";
 
-const SinglePost = (props) => {
-    return <h1>SinglePost Page</h1>
+const SinglePost = ({posts}) => {
+    const params = useParams()
+
+    const currentPost = useMemo(() => posts.find(post => {
+        return post.id === parseInt(params.id)
+    }), [params.id, posts])
+    
+    return( 
+    <div>
+        <h1>{currentPost.title}</h1>
+        <h2>{currentPost.body}</h2>
+        <Link to={'/'}>
+            <button>Go Back</button>
+        </Link>
+    </div>
+    )
 }
 
 export default SinglePost
